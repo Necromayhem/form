2afc354cfbee0ea38526829fadc08c4db973c7d3
Necromayhem <aleksanderkuklev1488@gmail.com>
2025-08-05 13:22:23 +0300

    qwe

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,18 @@
+import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    vue({
-      script: {
-        defineModel: true,
-        propsDestructure: true,
-      },
-    }),
+    vue(),
+    vueDevTools(),
   ],
-  build: {
-    emptyOutDir: true,  
-  }
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
 })
